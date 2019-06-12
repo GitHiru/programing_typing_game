27477ebe1a79cfd0185bebf50f90511c0b2ee4a3
huru_26 <w0rk.we6+sns@gmail.com>
2019-06-12 22:05:45 +0800

    KeyUpイベントハンドラ

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 'use strict';
 {
+  const words = [
+    '1test',
+    '2test',
+    '3test',
+    '4test',
+    '5test',
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];//ランダム取得
+  console.log(word);
+  let loc = 0;
+  let score = 0;
+  let miss = 0;
 
-let word = 'etc';
-const target = document.getElementById('target');
-targer.textContent = word;
+  const target = document.getElementById('target');
+  // console.log(target);
+  target.textContent = word;
 
+  function updateTarget(){
+    let placeholder = '';
+  }
+
+ // （keyupイベントハンドラ）
+ window.addEventListener('keyup', e =>{ //打鍵文字を取得
+   console.log(e.key);
+ });
 }
